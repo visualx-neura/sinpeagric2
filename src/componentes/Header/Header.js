dfde11a43dfe412081c0cae630489541fc2e1b9e
john jairo pinzon quintana <johnjairopinzonquintana@gmail.com>
2025-03-21 16:39:31 +0100

    se quito la url de productos

--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -12,8 +12,8 @@ function Header({ onNuevoVideoClick, onAdminClick, mostrarNuevoVideo, }) {
           <button onClick={onNuevoVideoClick} className="header__button">Nuevo Archivo</button>
         )}
         <button onClick={onAdminClick} className="header__button">Administrador</button>
-        <a href="https://www.tu-pagina-externa.com" target="_blank" rel="noopener noreferrer" className="header__button">Nuestros Productos</a>
-        <a href="https://www.tu-pagina-externa.com" target="_blank" rel="noopener noreferrer" className="header__button">Peliculas</a>
+        
+        <a href="https://pelistotal.vercel.app/" target="_blank" rel="noopener noreferrer" className="header__button">Peliculas</a>
       </nav>
     </header>
   );
